7857569841c5cb63523655660c91e993852f4e61
Masumi Kawasaki <geeknees@gmail.com>
2025-06-08 01:26:20 +0900

    Remove unnecessary blank lines in question_templates_test.rb for improved readability

--- test/system/question_templates_test.rb
+++ test/system/question_templates_test.rb
@@ -11,7 +11,7 @@ class QuestionTemplatesTest < ApplicationSystemTestCase
     fill_in "email_address", with: user.email_address
     fill_in "password", with: "password"
     click_on "Sign in"
-    
+
     # Wait for redirect and ensure we're signed in
     assert_text "Dashboard", wait: 5
   end
@@ -19,10 +19,10 @@ class QuestionTemplatesTest < ApplicationSystemTestCase
   test "shows rating options when rating type selected" do
     sign_in(@user)
     visit new_organization_question_template_path(@organization, locale: "en")
-    
+
     # Ensure we're on the correct page
     assert_text "New Template"
-    
+
     select I18n.t("questions.type.rating", locale: :en), from: "question_template[question_type]"
 
     assert_selector "#options-section", visible: true
