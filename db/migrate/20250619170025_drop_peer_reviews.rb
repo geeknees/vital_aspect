6045fffbf7032743d12d45d74bb1fa7a9701c090
Masumi Kawasaki <geeknees@gmail.com>
2025-06-20 02:04:55 +0900

    Refactor spacing in `drop_peer_reviews` migration file for consistency and readability.

--- db/migrate/20250619170025_drop_peer_reviews.rb
+++ db/migrate/20250619170025_drop_peer_reviews.rb
@@ -5,13 +5,13 @@ class DropPeerReviews < ActiveRecord::Migration[8.0]
       t.integer :reviewer_participant_id, null: false
       t.integer :reviewee_participant_id, null: false
       t.timestamps
-      
-      t.index [:evaluation_id, :reviewer_participant_id, :reviewee_participant_id], 
-              name: "index_peer_reviews_on_participants_and_evaluation", 
+
+      t.index [ :evaluation_id, :reviewer_participant_id, :reviewee_participant_id ],
+              name: "index_peer_reviews_on_participants_and_evaluation",
               unique: true
-      t.index [:evaluation_id], name: "index_peer_reviews_on_evaluation_id"
-      t.index [:reviewer_participant_id], name: "index_peer_reviews_on_reviewer_participant_id"
-      t.index [:reviewee_participant_id], name: "index_peer_reviews_on_reviewee_participant_id"
+      t.index [ :evaluation_id ], name: "index_peer_reviews_on_evaluation_id"
+      t.index [ :reviewer_participant_id ], name: "index_peer_reviews_on_reviewer_participant_id"
+      t.index [ :reviewee_participant_id ], name: "index_peer_reviews_on_reviewee_participant_id"
     end
   end
 end
